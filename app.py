4271212d59c078be7bf28cf6ec3d07a1a7228ae7
Zihad Bappy <zihadbappy@gmail.com>
2021-10-24 00:42:34 +0600

    untrack 2

---
+++ app.py
@@ -0,0 +1,103 @@
+from flask import Flask, jsonify, request, render_template,redirect, session
+from flask.helpers import url_for
+import traceback
+import sys
+import os
+from termcolor import colored
+import dns
+import json
+from bson import ObjectId   
+import sys
+import pymongo
+import logging
+from pymongo import MongoClient
+import certifi
+from bson import json_util
+from bson.json_util import default, dumps
+from werkzeug.utils import redirect
+from datetime import datetime
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)                                             
+
+import auth_routes
+from auth_routes import login_is_required
+
+ca = certifi.where()
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    client = pymongo.MongoClient(os.getenv('MONGO_URI'), tlsCAFile=ca)
+    db=client.UrbanBangla
+except pymongo.errors.ConnectionFailure as e:
+    print(colored(e, 'red'))
+
+# words =[{ "id": "0", 'word': 'nice lagsa', 'definition': "You're looking nice"}]
+
+@app.route("/")
+def get_words():
+    words=list(db.words.find({"status":"approved"}))
+    json_words= json.loads(json.dumps(words, default=json_util.default))
+    print(colored(json_words,'yellow'))
+    return render_template('home.html',json_words=json_words)
+    # return json.dumps(words, default=json_util.default)
+
+@app.route("/addword", methods=['POST'])
+def post_word():
+    try:
+        word=request.form['word']
+        definition=request.form['def']
+        exp=request.form['exp']
+        # email= session['email']
+        # userHandle=request.form['userHandle']
+
+        # print(colored("word={} , definition={} , exp={} , email={}, userHandle".format(word, definition, exp, email, userHandle), 'red'))
+        json= {"word":word,
+        "definition":definition,
+        "exp":exp,
+        "email": session["email"],
+        "userHandle": session["name"],
+        "upvote":"0",
+        "downvote":"0",
+        "status":"pending",
+        "postedDate": datetime.now()
+        }
+
+        print(colored(json, 'yellow'))
+        db.words.insert_one(json)
+        # return jsonify(result={"status": 200})
+        return redirect("/addword")
+    except:
+        traceback.print_exc(file=sys.stdout)
+
+
+@app.route("/addword", methods=['GET'])
+@login_is_required
+def word_form():
+    return render_template('addword.html')
+
+@app.route("/adminDashboard", methods=['GET'])
+def dash():
+    words=list(db.words.find({'status':'pending'}))
+    json_words= json.loads(json.dumps(words, default=json_util.default))
+    print(colored(json_words,'yellow'))
+    return render_template('adminDashboard.html',json_words=json_words)
+
+@app.route("/adminDashboard/<word_id>/", methods=['POST'])
+def approve_word(word_id):
+    oid = ObjectId(word_id)
+    print(colored(oid, 'green'))
+    # wordID= db.words.count_documents({"_id": {"$oid":"6162ef8d8dbc49ec785c61c8"}})
+    id= db.words.find({"_id": {"$eq": oid}})
+    id= json.loads(dumps(id, default=json_util.default))
+    print(colored(id,'red'))
+
+    if(id is not None):
+        db.words.update_one(
+        {"_id":{"$eq":oid}},
+        {'$set': {"status":"approved"}})
+    return redirect('/adminDashboard')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ auth_routes.py
@@ -0,0 +1,85 @@
+import collections
+from functools import wraps
+from flask.templating import render_template
+from termcolor import colored
+from app import app
+import os
+import pathlib
+import requests
+from flask import Flask, session, abort, redirect, request
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
+# app.secret_key = "mySecretKeyLol"
+
+GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/user/callback"
+)
+
+def login_is_required(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return render_template('unauthorized.html')  # Authorization required
+        else:
+            return function()
+
+    return wrapper
+
+
+@app.route('/user', methods=['GET'])
+def loginpage():
+    return render_template('login.html')
+
+
+@app.route('/user/login', methods=['GET'])
+def userlogin():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+@app.route('/user/callback', methods=['GET'])
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+    print(colored(session["state"],'red'))
+    print(colored(request.args["state"],'red'))
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    session["email"] = id_info.get("email")
+    session["picture"] = id_info.get("picture")
+    print(colored(id_info,'red'))
+    return redirect("/user/protected_area")
+
+@app.route('/user/logout', methods=['GET'])
+def userlogout():
+    session.clear()
+    return redirect("/user")
+
+@app.route('/user/protected_area', methods=['GET'])
+@login_is_required
+def protected_area():
+    return redirect('/addword')
